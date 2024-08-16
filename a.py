d1875b729ae2cf49d3b5254de104844caa49860e
harmesh-art <harmeshbehera13@gmail.com>
2024-08-16 19:54:33 +0530

    integer declared

--- a.py
+++ a.py
@@ -1,2 +1,6 @@
 print("Hello World")
-#
+# "git add ."-> to add all files to the git hub
+#"git add a.py"---->>> to add a particular file
+#"git commit -m "message""  ---->>> to save the changes
+#"git push"  ---->> to push the files
+a=10
