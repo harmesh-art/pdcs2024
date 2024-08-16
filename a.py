57ab22f51b90ad3be8d47173e70e303fbb50e4e5
harmesh-art <harmeshbehera13@gmail.com>
2024-08-16 20:14:57 +0530

    b file

--- a.py
+++ a.py
@@ -3,4 +3,5 @@ print("Hello World")
 #"git add a.py"---->>> to add a particular file
 #"git commit -m "message""  ---->>> to save the changes
 #"git push"  ---->> to push the files
+#git checkout -b "branch_name"---->>> to create a new branch
 a=10
